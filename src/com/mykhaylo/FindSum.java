0578ed00536b49ea777e67eaf69ff56149c33304
Mykhailo <m_musienko@outlook.com>
2022-01-21 21:08:03 +0200

    Add  FindSum

---
+++ src/com/mykhaylo/FindSum.java
@@ -0,0 +1,24 @@
+package com.mykhaylo;
+
+public class FindSum {
+    public static void main(String[] args) {
+        print(findSum(100.0, 1));
+    }
+
+    /*
+    1) В банк положили s денег. Какой станет сумма вклада через n лет, если процент 1,5% добавляется
+       к сумме вклада ежемесячно. (сложный процент)
+       Написать метод который принимает 2 параметра - s, n и возвращает сумму денег на счету.
+     */
+    public static double findSum(double s, int n) {
+        int months = n * 12;
+        for (int i = 0; i < months; i++) {
+            s += s * 0.015d;
+        }
+        return s;
+    }
+
+    public static void print(double value) {
+        System.out.printf("%.2f", value);
+    }
+}
