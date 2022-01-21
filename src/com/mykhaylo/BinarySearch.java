784bf349833493e5aa3cdb58cbc4ee44412f8fcf
Mykhailo <m_musienko@outlook.com>
2022-01-21 22:20:17 +0200

    A bug fix in BinarySearch

--- src/com/mykhaylo/BinarySearch.java
+++ src/com/mykhaylo/BinarySearch.java
@@ -4,8 +4,8 @@ import java.util.Arrays;
 
 public class BinarySearch {
     public static void main(String[] args) {
-        print(binarySearch1(new int[]{1, 8, 10, 15, 20, 30, 50, 51, 52, 53}, 51, 0, 9));
-        print(binarySearch2(new int[]{1, 8, 10, 15, 20, 30, 50, 51, 52, 53}, 51));
+        print(binarySearch1(new int[]{1, 8, 10, 15, 20, 30, 50, 51, 52, 53}, 70, 0, 9));
+        print(binarySearch2(new int[]{1, 8, 10, 15, 20, 30, 50, 51, 52, 53}, 53));
     }
 
     /*
@@ -13,8 +13,8 @@ public class BinarySearch {
         Входящие параметры: массив и число.
         (считаем что массива который нам передали отсортирован, проверять это не нужно)
      */
-    public static int binarySearch1(int[] sortedArray, int key, int low, int high) {
-        int index = 0;
+    public static boolean binarySearch1(int[] sortedArray, int key, int low, int high) {
+        int index = -1;
 
         while (low <= high) {
             int mid = low + ((high - low) / 2);
@@ -27,14 +27,17 @@ public class BinarySearch {
                 break;
             }
         }
-        return index;
+        if (index == -1) {
+            return false;
+        }
+        return true;
     }
 
-    public static int binarySearch2(int[] sortedArray, int key) {
-        return Arrays.binarySearch(sortedArray, key);
+    public static boolean binarySearch2(int[] sortedArray, int key) {
+        return Arrays.binarySearch(sortedArray, key) >= 0;
     }
 
-    public static void print(int index) {
-        System.out.println(index);
+    public static void print(boolean value) {
+        System.out.println(value);
     }
 }
